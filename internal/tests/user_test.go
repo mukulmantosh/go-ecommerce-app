9f1a936413e4ad1574e2f1f46bce0fd0acd64a95
Mukul Mantosh <mukulmantosh@yahoo.com>
2023-08-13 15:46:00 +0530

    Tests

---
+++ internal/tests/user_test.go
@@ -0,0 +1,38 @@
+package tests
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/mukulmantosh/go-ecommerce-app/internal/database"
+	"github.com/mukulmantosh/go-ecommerce-app/internal/server"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+var userJSON = `{
+  "username": "mukulbasasaba111",
+  "password": "mukul123",
+  "first_name": "mukul",
+  "last_name": "mantosh"
+}
+`
+
+var testDb, _ = database.NewDBClient()
+var service = server.NewServer(testDb)
+
+func TestAddUser(t *testing.T) {
+	t.Run("should return 200 status ok", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodPost, "/user", strings.NewReader(userJSON))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		// Assertions
+		if assert.NoError(t, service.AddUser(c)) {
+			assert.Equal(t, http.StatusCreated, rec.Code)
+		}
+	})
+}
